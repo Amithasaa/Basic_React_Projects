8d51bf185aaa7763032c0bdca56c04104eebd8f6
Amithasaa <amithaamitha290@gmail.com>
2024-12-26 12:50:18 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,17 @@
-
-
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Greeting from './Greeting';
 function App() {
   return (
-    <h1>hello</h1>
+    <div>
+    <h1>hello from ReactJS</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'}/>
+   {/* <Home name = {'Pooja'} age = {21}/>
+   <About />
+   <Contact /> */}
+    </div>
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <>
+        <h1>Header content</h1>
+        <header>Some header content</header>
+        </>
+    );
+}
+export default Header; 
